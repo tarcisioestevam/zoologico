8abe633242a40945ac0fd212ecad7929d804a3e0
Tarcísio Estevam <tarcisioestevam@gmail.com>
2023-06-17 19:05:15 -0300

    Adicionar a rota roedores

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -54,4 +54,10 @@ class HelloController
     {
         return new Response('Olá, caninos!');
     }
+
+    #[Route(path: '/roedores')]
+    public function roedor(): Response
+    {
+        return new Response('Olá, roedores!');
+    }
 }
