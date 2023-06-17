85f1502c30226d3fc51b9aee4a8bd9ddfe3b2948
Tarcísio Estevam <tarcisioestevam@gmail.com>
2023-06-17 19:40:16 -0300

    Adicionar rota coringa

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
 
 class HelloController
 {
@@ -13,51 +14,11 @@ class HelloController
         return new Response('Olá!');
     }
 
-    #[Route(path: '/dinossauros')]
-    public function dinossauro(): Response
+    #[Route(path: '/animal/{slug}')]
+    public function animal(string $slug = null): Response
     {
-        return new Response('Olá, dinossauro!');
-    }
-
-    #[Route(path: '/mamiferos')]
-    public function mamifero(): Response
-    {
-        return new Response('Olá, mamíferos!');
-    }
-
-    #[Route(path: '/invertebrados')]
-    public function invertebrado(): Response
-    {
-        return new Response('Olá, invertebrados!');
-    }
-
-    #[Route(path: '/moluscos')]
-    public function molusco(): Response
-    {
-        return new Response('Olá, moluscos!');
-    }
-
-    #[Route(path: '/insetos')]
-    public function inseto(): Response
-    {
-        return new Response('Olá, insetos!');
-    }
-
-    #[Route(path: '/felinos')]
-    public function felino(): Response
-    {
-        return new Response('Olá, felinos!');
-    }
-
-    #[Route(path: '/caninos')]
-    public function canino(): Response
-    {
-        return new Response('Olá, caninos!');
-    }
-
-    #[Route(path: '/roedores')]
-    public function roedor(): Response
-    {
-        return new Response('Olá, roedores!');
+        $newSlug = str_replace('-', ' ', $slug);
+        $newSlug = u($newSlug)->title(true);
+        return new Response('Olá, ' . $newSlug . '!');
     }
 }
